f2e10b1ec1fcf435151da783b025c7da140f56cb
DabAZ <dabaz@dabaz.me>
2025-09-10 02:23:41 +0800

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,15 +3,11 @@ import Image from 'next/image';
 import { Download } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
 
 const Header = () => {
   return (
     <header className="fixed inset-x-0 top-0 z-50 flex flex-col">
-      <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8 bg-white dark:bg-black">
+      <nav aria-label="Global" className="flex items-center justify-between p-4 lg:px-8 bg-neutral-100 dark:bg-neutral-900 border-b border-neutral-300 dark:border-neutral-700">
         <div className="flex lg:flex-1">
           <Link href="/" className="-m-1.5 p-1.5 flex flex-row space-x-2">
             <span className="sr-only">DabLRC</span>
@@ -34,6 +30,14 @@ const Header = () => {
             </h2>
           </Link>
         </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <Link href="/edit" className="font-semibold text-neutral-900 dark:text-white">
+            Edit
+          </Link>
+          <Link href="/sync" className="font-semibold text-neutral-900 dark:text-white">
+            Sync
+          </Link>
+        </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Button className="text-sm/6 font-semibold">
             Save
@@ -41,64 +45,6 @@ const Header = () => {
           </Button>
         </div>
       </nav>
-      <div className="flex items-center justify-center p-2 lg:px-8 bg-white dark:bg-black">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <Tabs defaultValue="account">
-            <TabsList>
-              <TabsTrigger value="account">Account</TabsTrigger>
-              <TabsTrigger value="password">Password</TabsTrigger>
-            </TabsList>
-            <TabsContent value="account">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Account</CardTitle>
-                  <CardDescription>
-                    Make changes to your account here. Click save when you&apos;re
-                    done.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="grid gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="tabs-demo-name">Name</Label>
-                    <Input id="tabs-demo-name" defaultValue="Pedro Duarte" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="tabs-demo-username">Username</Label>
-                    <Input id="tabs-demo-username" defaultValue="@peduarte" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save changes</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-            <TabsContent value="password">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Password</CardTitle>
-                  <CardDescription>
-                    Change your password here. After saving, you&apos;ll be logged
-                    out.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent className="grid gap-6">
-                  <div className="grid gap-3">
-                    <Label htmlFor="tabs-demo-current">Current password</Label>
-                    <Input id="tabs-demo-current" type="password" />
-                  </div>
-                  <div className="grid gap-3">
-                    <Label htmlFor="tabs-demo-new">New password</Label>
-                    <Input id="tabs-demo-new" type="password" />
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <Button>Save password</Button>
-                </CardFooter>
-              </Card>
-            </TabsContent>
-          </Tabs>
-        </div>
-      </div>
     </header>
   )
 };
